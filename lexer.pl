3f639a37db063f154f527e9b360c30abacb7e3ae
ccr185 <camilo.correa-restrepo@univ-paris1.fr>
2023-11-07 20:35:53 +0100

    working lexer parser for uvl v1

---
+++ lexer.pl
@@ -0,0 +1,223 @@
+:- module(lexer, [lex_uvl/2]).
+:- set_prolog_flag(double_quotes, string).
+:- set_prolog_flag(encoding, utf8).
+:- use_module(library(dcg/basics)).
+:- use_module(library(dcg/high_order)).
+:- use_module(library(pio)).
+%:- use_module(library(regex)).
+:- use_module(library(pcre)).
+:- use_module(library(clpfd)).
+:- use_module(library(lists)).
+:- use_module(library(yall)).
+:- use_module(library(apply)).
+:- use_module(library(apply_macros)).
+
+lex_uvl(Tokens, File) :-
+    phrase_from_file(tokenize(TokensCollected), File),
+    flatten(TokensCollected, Tokens).
+
+tokenize(Tokens) -->
+    tokenize_([0],Tokens).
+
+%% FIXME: This definition and the one at the bottom seem to conflict.
+tokenize_(RemainingIndentStack, DedentTokens) --> eos, !,
+    {
+        format("Remaining Indent Stack: ~w", [RemainingIndentStack]),
+        length(RemainingIndentStack, Length),
+        dedentStack(Length, RemainingIndentStack, DedentTokens)
+    }.
+tokenize_([IndentLevel|Is], [LineToks|Tokens]) -->
+    tokenize_line(LineIndent, LineToks0), !,
+    {
+        % Check for equal indent level
+        (
+            LineIndent =:= IndentLevel
+        ) -> (
+            % Case 1: Indentation level is the same
+            % do not generate indent/dedent token
+            LineToks = LineToks0,
+            IndentStack = [IndentLevel|Is]
+        ) ; (
+            % Check for increase/decrease in indent
+            % level
+            (
+                LineIndent #> IndentLevel
+            ) -> (
+                % Case 2: Indentation is higher than stack,
+                % emit an indent token before the line tokens
+                LineToks = [indent | LineToks0],
+                IndentStack = [LineIndent,IndentLevel|Is],
+                writeln("emit indent token")
+            ) ; (
+                % Case 3: Indentation is lower than stack
+                % check that this level exists
+                % emit a dedent token
+                % TODO: Add error checking and throw an error if
+                % there's a problem.
+                member(LineIndent, Is),
+                drop_higher(Is, LineIndent, NewStack, NPopped0),
+                %Since we are 0 indexing and we are
+                %dropping with the last indent already removed
+                %we always drop N+1
+                NPopped #= NPopped0 + 1,
+                length(Dedents, NPopped),
+                maplist([X]>>(X = dedent), Dedents),
+                append(Dedents, LineToks0, LineToks),
+                %LineToks = [dedent | LineToks0],
+                IndentStack = NewStack,
+                format("emit ~w dedent tokens \n", [NPopped])
+            )
+        )
+    },
+    tokenize_(IndentStack, Tokens).
+% TODO: emit dedent tokens corresponding
+% to the remaining indent level left on stack
+tokenize_(RemainingIndentStack, DedentTokens) -->
+    [], !,
+    {
+        format("Remaining Indent Stack: ~w", [RemainingIndentStack]),
+        length(RemainingIndentStack, Length),
+        dedentStack(Length, RemainingIndentStack, DedentTokens)
+    }.
+
+%We are certain that the element is in the list.
+drop_higher(OrigStack, Elem, NewStack, Pos) :-
+    nth0(Pos,OrigStack, Elem),
+    drop(Pos, OrigStack, NewStack), !.
+
+%Based on https://www.swi-prolog.org/pldoc/doc/_SWI_/library/dialect/hprolog.pl
+drop(0, LastElements, LastElements) :- !.
+drop(N, [_|Tail], LastElements) :-
+    N #> 0, N1 #= N- 1, drop(N1,Tail,LastElements).
+
+dedentStack(1, _, []) :- !.
+dedentStack(N, [_|TA], [dedent|TB]) :-
+    N #> 0, N2 #= N - 1,
+    dedentStack(N2, TA, TB).
+
+tokenize_line(N, Ts) -->
+    indent(N),
+    line_tokens(Ts).
+    %(`\n`|`\r\n`).
+
+indent(N) --> indent_(0,N).
+
+indent_(N0,N) --> ` `,  !, indent_(N1,N), {N1 #= N0 + 1}.
+indent_(N0,N) --> `\t`, !, indent_(N1,N), {N1 #= N0 + (8 - (N0 mod 8))}.
+indent_(N0,N0) --> [], !.
+
+line_tokens([]) --> eol, !.
+line_tokens([T|Ts]) -->
+    next_token(T), !, whites, line_tokens(Ts).
+line_tokens([]) --> [], !.
+
+next_token(Token) -->
+    (keyword(Token), !, {format("read keyword ~w \n", [Token])}) |
+    (double_char(Token), !, {format("read double_char ~w \n", [Token])}) |
+    (single_char(Token), !, {format("read single_char ~w \n", [Token])}) |
+    (literal(Token), !, {format("read literal ~w \n", [Token])}).
+
+% Keywords
+keyword(Token) -->
+    ("group-cardinality", {Token = group_card}, !) |
+    ("feature-cardinality", {Token = feature_card}, !) |
+    ("aggregate-function", {Token = agg_fun}, !) |
+    ("string-constraints", {Token = string_constraints}, !),
+    ("namespace", {Token = namespace}, !) |
+    ("include", {Token = include}, !) |
+    ("imports", {Token = imports}, !) |
+    ("features", {Token = features}, !) |
+    ("as", {Token = as}, !) |
+    ("or", {Token = or_group}, !) |
+    ("alternative", {Token = alternative}, !) |
+    ("optional", {Token = optional}, !) |
+    ("mandatory", {Token = mandatory}, !) |
+    ("cardinality", {Token = cardinality}, !) |
+    ("constraints", {Token = constraints}, !) |
+    ("constraint", {Token = constraint}, !) |
+    ("sum", {Token = sum}, !) |
+    ("avg", {Token = avg}, !) |
+    ("len", {Token = len}, !) |
+    ("floor", {Token = floor}, !) |
+    ("ceil", {Token} = ceil, !) |
+    ("String", {Token = string_t}, !) |
+    ("Integer", {Token = integer_t}, !) |
+    ("Real", {Token = real_t}, !) |
+    ("Boolean", {Token = boolean_t}, !) |
+    ("Arithmetic", {Token = arithmetic_t}, !) |
+    ("Type", {Token = type_t}, !) |
+    ("true", {Token = lit_true}, !),
+    ("false", {Token = lit_false}, !).
+
+%Double Char Toks
+double_char(Token) -->
+    ("..", {Token = range}, !) |
+    ("<=>", {Token = equivalence}, !) |
+    ("=>", {Token = impl}, !) |
+    ("==", {Token = equal}, !) |
+    ("<=", {Token = lte}, !) |
+    (">=", {Token = gte}, !) |
+    ("!=", {Token = neq}, !) |
+    ("//", {Token = comment_start}, !) |
+    ("/*", {Token = ml_comment_start}, !) |
+    ("*/", {Token = ml_comment_end}, !).
+
+single_char(Token) -->
+    (`,`, {Token = comma}, !) |
+    (`.`, {Token = dot}, !) |
+    (`!`, {Token = not}, !) |
+    (`&`, {Token = and}, ! ) |
+    (`|`, {Token = or}, !) |
+    (`<`, {Token = lt}, !) |
+    (`>`, {Token = gt}, !) |
+    (`*`, {Token = mul}, !) |
+    (`+`, {Token = add}, !) |
+    (`-`, {Token = sub}, !) |
+    (`/`, {Token = div}, !) |
+    ("(", {Token = lparen}, !) |
+    (")", {Token = rparen}, !) |
+    ("[", {Token = lbracket}, !) |
+    ("]", {Token = rbracket}, !) |
+    ("{", {Token = lbrace}, ! ) |
+    ("}", {Token = rbrace}, !).
+
+literal(float(Token)) --> float(Token), !.
+literal(integer(Token)) --> integer(Token), !.
+literal(Token) -->
+    string_without(".,-(){}[] \t\n\r", Codes),
+    {
+        \+ length(Codes, 0),
+        atom_codes(Atom, Codes),
+        atom_string(Atom, Str),
+        (
+            id_strict(Str)
+        ) -> (
+            Token = id_strict(Str)
+        ) ; (
+            (
+                id_not_strict(Str)
+            ) -> (
+                Token = id_not_strict(Str)
+            ) ; (
+                (
+                    normal_string(Str)
+                ) -> (
+                    Token = normal_string(Str)
+                ) ; (
+                    throw("Unrecognized string format")
+                )
+            )
+        )
+    }, !.
+
+id_strict(Str) :-
+    re_match(
+        "^[a-zA-Z]([a-zA-Z0-9_]|#|§|%|\\|\\?|\'|ä|ü|ö|ß|;)*(?![\\)\\]\\}])$",
+        Str,[anchored(true),utf(true)]
+    ).
+id_not_strict(Str) :- re_match("\"[^\r\n\".]+\"$", Str, [anchored(true),utf(true)]).
+normal_string(Str) :- re_match("\'[^\r\n\'.]+\'$", Str, [anchored(true),utf(true)]).%% %
+
+%id_strict(Str) :- Str =~ '[a-zA-Z]([a-zA-Z0-9_]|#|§|%|\\?|\\|\'|ä|ü|ö|ß|;)*(?![\)\]\}])$', !.
+%id_not_strict(Str) :- Str =~ '\"[^\r\n\".]+\"$', !.
+%normal_string(Str) :- Str =~  '\'[^\r\n\'.]+\'$', !.
--- uvl.pl
+++ uvl.pl
@@ -1,26 +1,167 @@
-:- set_prolog_flag(double_quotes, chars).
+:- set_prolog_flag(double_quotes, string).
 :- use_module(library(dcg/basics)).
 :- use_module(library(dcg/high_order)).
+:- use_module(lexer).
+:- use_module(library(tabling)).
 
-parse(String, AST) :-
-    phrase((uvl(AST),eos), String).
+parse(File, AST, Stop) :-
+    lex_uvl(Tokens, File), !,
+    phrase(uvl(AST), Tokens, []).
 
 uvl(ast(H,F,C)) -->
     header(H),
-    features(F),
-    constraints(C).
+    optional(features(F),{F = nil}),
+    optional(constraints(C),{C = nil}).
 
 header(header(namespace(N),includes(In),imports(Im))) -->
-    optional((namespace(N),op_nl),[]),
-    optional((includes(In),op_nl),[]),
-    optional((imports(Im),op_nl),[]).
+    optional(namespace(N),{N = []}),
+    optional(includes(In),{In = []}),
+    optional(imports(Im),{Im = []}).
 
-namespace(N) --> "namespace", blanks, string(N).
+namespace(N) --> [namespace], reference(N), !.
+reference(N) --> ([id_strict(N)], !) | ([id_not_strict(N)], !).
 
-includes(I) --> "include ", blanks, remainder(I).
-imports(I) --> [].
-features(F) --> [].
-constraints(C) --> [].
+includes(Is) --> [include, indent], includes_(Is), [dedent].
+includes_([I|Is]) --> language_level(I), !, includes_(Is).
+includes_([]) --> [].
 
-op_nl --> optional("\n",[]).
-op_nl --> optional("\r\n",[]).
+language_level(lang_level(Major,Minor)) -->
+    major_level(Major), optional(minor_level(Minor), {Minor = []}).
+
+major_level(Major) -->
+    ([boolean_t], {Major = boolean_level}, ! ) |
+    ([arithmetic_t], {Major = arithmetic_level}, ! ) |
+    ([type_t], {Major = type_level}, ! ).
+minor_level(Minor) -->
+    [dot], !, (
+        ([group_card], {Minor = group_card}, !) |
+        ([feature_card], {Minor = feature_card}, !) |
+        ([agg_fun], {Minor = agg_fun}, !) |
+        ([string_constraints], {Minor = string_constraints}, !) |
+        ([mul], {Minor = minor_all}, !)
+    ).
+
+imports(Is) --> [imports, indent], imports_(Is), [dedent].
+imports_([import(NS,Alias)|Is]) -->
+    reference(NS),
+    optional(([as], reference(Alias)), {Alias = []}), !,
+    imports_(Is).
+imports_([]) --> [].
+
+features(Fs) --> [features, indent], features_(Fs), [dedent].
+features_(
+    [feature(
+        name(N), type(FeatureType), cardinality(Card),
+        attributes(Attrs), group(Gs)
+    )|Fs]
+) -->
+    optional(feature_type(FeatureType), {FeatureType = boolean}),
+    reference(N),
+    optional(feature_cardinality(Card), {Card = nil}),
+    optional(attributes(Attrs), {Attrs = nil}),
+    optional(([indent], groups(Gs), [dedent]), {Gs = nil}), !,
+    features_(Fs).
+features_([]) --> [].
+
+feature_type(boolean) --> [boolean_t], !.
+feature_type(integer) --> [integer_t], !.
+feature_type(string) --> [string_t], !.
+feature_type(real) --> [real_t], !.
+
+feature_cardinality(card(From,To)) -->
+    [cardinality, lbracket, integer(From)],
+    optional(([range], ([integer(To)] | ([mul], {To = inf}))), {To = nil}),
+    [rbracket].
+cardinality(card(From,To)) -->
+    [lbracket, integer(From)],
+    optional(([range], ([integer(To)] | ([mul], {To = inf}))), {To = nil}),
+    [rbracket].
+
+attributes(Attrs) --> [lbrace], attributes_(Attrs), [rbrace].
+attributes_([A|As]) -->
+    (value_attr(A) | constraint_attr(A)), [comma], !, attributes_(As).
+attributes_([A]) --> (value_attr(A) | constraint_attr(A)), !.
+attributes_([]) --> [], !.
+
+value_attr(attr(key(K),val(V))) -->
+    id(K), optional(value(V), {V = nil}).
+
+value(bool(B)) --> [boolean(B)], !.
+value(integer(I)) --> [integer(I)], !.
+value(float(F)) --> [float(F)], !.
+value(string(S)) --> [normal_string(S)], !.
+value(attributes(Attrs)) --> attributes(Attrs), !.
+value(vector(V)) --> [lbracket], vector(V), [rbracket], !.
+
+vector([V|Vs]) --> value(V), [comma], !, vector(Vs).
+vector([V]) --> value(V), !.
+vector([]) --> [], !.
+
+groups([G|Gs]) --> group(G), !, groups(Gs).
+groups([]) --> [].
+
+group(or_group(S)) -->  [or_group], group_spec(S), !.
+group(alternative(S)) --> [alternative], group_spec(S), !.
+group(optional(S)) --> [optional], group_spec(S), !.
+group(mandatory(S)) --> [mandatory], group_spec(S), !.
+group(cardinality(Card,S)) --> cardinality(Card), group_spec(S), !.
+
+group_spec(Fs) --> [indent], features_(Fs), {length(Fs,L), L > 0}, [dedent].
+
+constraint_attr(C) --> [constraint], constraint(C), !.
+constraint_attr(Cs) --> [constraints], constraint_list(Cs), !.
+
+:- table constraint/3.
+
+constraint(equation(E)) --> equation(E), !.
+constraint(literal_constraint(C)) --> reference(C).
+constraint(paren_constraint(C)) --> [lparen], constraint(C), [rparen], !.
+constraint(not_constraint(C)) --> [not], constraint(C), !.
+constraint(and_constraint(C1,C2)) --> constraint(C1), [and], constraint(C2), !.
+constraint(or_constraint(C1,C2)) --> constraint(C1), [or], constraint(C2), !.
+constraint(impl_constraint(C1,C2)) --> constraint(C1), [impl], constraint(C2), !.
+constraint(eq_constraint(C1,C2)) --> constraint(C1), [equivalence], constraint(C2), !.
+
+constraint_list(Cs) --> [lbracket], constraint_list_(Cs), [rbracket].
+constraint_list_([C|Cs]) --> constraint(C), [comma], !, constraint_list_(Cs).
+constraint_list_([C]) --> constraint(C), !.
+constraint_list_([]) --> [], !.
+
+constraints(Cs) --> [constraints, indent], constraints_(Cs), [dedent].
+constraints_([C|Cs]) --> constraint(C), !, constraints_(Cs).
+constraints_([]) --> [].
+
+:- table equation/3.
+
+equation(equal(E1,E2)) --> expression(E1), [equal], expression(E2), !.
+equation(lt(E1,E2)) --> expression(E1), [lt], expression(E2), !.
+equation(gt(E1,E2)) --> expression(E1), [gt], expression(E2), !.
+equation(lte(E1,E2)) --> expression(E1), [lte], expression(E2), !.
+equation(gte(E1,E2)) --> expression(E1), [gte], expression(E2), !.
+equation(neq(E1,E2)) --> expression(E1), [neq], expression(E2), !.
+
+:- table expression/3.
+
+expression(E) --> ([float(E)] | [integer(E)] | [string(E)]), !.
+expression(E) --> aggregate_function(E), !.
+expression(E) --> reference(E).
+expression(sub_expression(E)) --> [lparen], expression(E), [rparen].
+expression(add(E1,E2)) --> expression(E1), [add], expression(E2), !.
+expression(sub(E1,E2)) --> expression(E1), [sub], expression(E2), !.
+expression(mul(E1,E2)) --> expression(E1), [mul], expression(E2), !.
+expression(div(E1,E2)) --> expression(E1), [div], expression(E2), !.
+
+aggregate_function(sum(ref(R),ref_op(ROP))) -->
+    [sum, lparen], optional((reference(ROP),[comma]), {ROP = nil}),
+    reference(R), [rparen], !.
+aggregate_function(avg(ref(R),ref_op(ROP))) -->
+    [avg, lparen], optional((reference(ROP),[comma]), {ROP = nil}),
+    reference(R), [rparen], !.
+aggregate_function(len(ref(R))) -->
+    [len, lparen], reference(R), [rparen], !.
+aggregate_function(floor(ref(R))) -->
+    [floor, lparen], reference(R), [rparen], !.
+aggregate_function(ceil(ref(R))) -->
+    [ceil, lparen], reference(R), [rparen], !.
+
+id(ID) --> [id_strict(ID)] | [id_not_strict(ID)].
